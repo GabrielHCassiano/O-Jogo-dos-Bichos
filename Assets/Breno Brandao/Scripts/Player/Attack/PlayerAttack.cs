3793c0d3f881f28d0ae6c42bc54ce732e6cd679b
Breno Brandão <brenoow@proton.me>
2024-03-27 14:38:30 -0300

    add main input system and animators/battle system

--- Assets/Breno Brandao/Scripts/Player/Attack/PlayerAttack.cs
+++ Assets/Breno Brandao/Scripts/Player/Attack/PlayerAttack.cs
@@ -7,16 +7,61 @@ public class PlayerAttack : MonoBehaviour
 
     [SerializeField] private float attackCooldown;
     private Animator anim;
+    public Transform balaoPoint;
+    public GameObject balaoPrefab;
+    public float attackspeed = 10;
 
+    //create an logic to attack others players or enemies in unity    //create an logic to attack others players or enemies in unity
+    public void Attack()
+    {
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var bullet = Instantiate(balaoPrefab, balaoPoint.position, balaoPoint.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = balaoPoint.right * attackspeed;
+
+        }
+
+        if (Time.time >= attackCooldown)
+        {
+            attackCooldown = Time.time + 1f;
+        }
+    }
+    // Start is called before the first frame update
 
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+
+        Attack();
+
     }
+
+
+
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Hit Enemy");
+            anim.Play("BalaoVerde");
+            Destroy(collision.gameObject);
+        }
+    }
+    
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit Enemy");
+            anim.Play("BalaoVerde");
+            Destroy(collision.gameObject);
+        }
+    }
+
 }
--- Assets/Breno Brandao/Scripts/Player/PlayerController.cs
+++ Assets/Breno Brandao/Scripts/Player/PlayerController.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class PlayerController : MonoBehaviour
 {
-
     public float speed = 5;
-    private Vector2 movementInput;
-
+    private InputManager inputManager;
     void Update()
     {
 
-        transform.Translate(new Vector2(movementInput.x,movementInput.y) * speed * Time.deltaTime);
+        if (GetComponent<PlayerID>().inputManager != null)
+            inputManager = GetComponent<PlayerID>().inputManager;
+        else
+            return;
+
+        transform.Translate(new Vector2(inputManager.moveDir.x, inputManager.moveDir.y) * speed * Time.deltaTime);
     }
-
-    public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
-
+    //public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
+    
     
 }
